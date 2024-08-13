c06b444dacad55c2c13a70548e18869709a9e5b5
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-08-13 13:46:51 +0300

    added the gallery carousel arrows

--- src/components/GalleryCarousel.vue
+++ src/components/GalleryCarousel.vue
@@ -9,15 +9,14 @@ defineProps({
 })
 </script>
 
-
 <template>
     <Carousel class="w-full h-full mx-auto mb-[16px]">
         <Slide v-for="(item, key) in images" :key="key" class="rounded-[2px] my-10">
-            <img :src="isEditorial ? item.image : item" :alt="'Bissar Concepts -' + isEditorial ? item.title : alt" width="1440" height="568" center cover responsive loading="eager"/>
+            <img :src="isEditorial ? item.image : item" :alt="'Bissar Concepts - ' + (isEditorial ? item.title : alt)" width="1440" height="568" center cover responsive loading="eager"/>
         </Slide>
         <template #addons>
-            <Navigation/>
-            <Pagination/>              
+            <Navigation />
+            <Pagination />              
         </template>
     </Carousel>
 </template>
@@ -28,5 +27,21 @@ defineProps({
     bottom: 1%;
     left: 50%;
     transform: translateX(-50%);
+}
+
+.carousel__arrow--prev,
+.carousel__arrow--next {
+    background: rgba(0, 0, 0, 0.5);
+    color: #fff;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.carousel__arrow--prev {
+    left: 20px;
+}
+
+.carousel__arrow--next {
+    right: 20px;
 }
 </style>
