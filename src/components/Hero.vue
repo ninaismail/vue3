83add34f6adfd453900ecec7de54d69ebc1e0912
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-08-20 22:56:09 +0300

    centered things

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@ import Socials from '../components/Socials.vue'
 <template>
   <section class="relative flex flex-col justify-center w-full h-screen lg:max-h-[721px] items-between mt-[90px]">
     <img src="/images/hero/hero.webp" alt="Bissar Concepts" center cover responsive loading="eager" class="aspect-[2/1] absolute inset-0 z-[-1] w-full h-full"/>
-    <div class="flex flex-col justify-between w-11/12 mx-auto 2xl:w-8/12 lg:w-10/12">
+    <div class="flex flex-col justify-between w-11/12 h-full py-20 mx-auto 2xl:w-8/12 lg:w-10/12">
       <div class="space-y-3 mb-[64px]">
         <h1 class="text-white uppercase lg:text-[64px] md:text-[52px] text-[40px] font-[700] leading-tight">
           Home <br class="max-lg:hidden"/>of Luxury
@@ -20,8 +20,8 @@ import Socials from '../components/Socials.vue'
         :exactActiveClass="'bg-olive text-white'"
         class="w-fit cursor-pointer px-4 py-3 font-[400] text-center rounded-[2px] bg-white hover:bg-olive hover:text-white uppercase"
         >View Projects</RouterLink>
+    <Socials class="mt-auto mb-0"/>
     </div>
-    <Socials class="mx-auto absolute top-[90%] lg:left-[16.67%] left-[6%] transform lg:translate-x-[16.67%]"/>
   <svg width="5" height="116" viewBox="0 0 5 116" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute mx-auto transform translate-x-1/2 top-[93%] left-1/2 z-[2]">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M1.66675 0.85498H2.77786V114.86H1.66675V0.85498Z" fill="#CCBB99" stroke="#CCBB99"/>
   </svg> 
