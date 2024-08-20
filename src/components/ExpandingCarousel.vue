006333c3e84749c30ab91afdb4b196de4dd35ff9
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-08-20 11:31:56 +0300

    3

--- src/components/ExpandingCarousel.vue
+++ src/components/ExpandingCarousel.vue
@@ -68,6 +68,6 @@ onMounted(() => {
   position: static !important;
   height: auto !important;
   margin: 0 10px !important;
-  align-items: normal !important;
+  align-items: unset !important;
 }
 </style>
