a11d5045031b5a0e4692d932ea75d0663907b903
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-08-23 20:31:07 +0200

    fixed the banners

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,14 +3,15 @@ import Socials from '../components/Socials.vue'
 
 </script>
 <template>
-  <section class="relative flex flex-col justify-center w-full 2xl:min-h-screen h-full items-between mt-[86px] bg-cover bg-center aspect-2/1 bg-hero">
+  <section class="relative flex items-center justify-center mt-[86px] min-h-[40vh] lg:min-h-[60vh] 2xl:min-h-[70vh] bg-cover bg-center aspect-2/1 bg-hero">
     <!-- <img src="/images/hero/hero.webp" alt="Bissar Concepts" loading="eager" class="aspect-[2/1] absolute inset-0 z-[-1] w-full h-full"/> -->
-    <div class="flex flex-col w-11/12 h-full py-40 mx-auto lg:w-10/12">
-      <div class="sm:space-y-3 2xl:mb-[64px] mb-[32px]">
+    <div class="relative top-0 flex flex-col justify-between w-11/12 mx-auto space-y-3 transform sm:space-y-3 lg:w-10/12">
+      <div class="w-full sm:space-y-3 2xl:mb-[64px] sm:mb-[32px]">
         <h1 class="text-white uppercase lg:text-[48px] md:text-[32px] text-[14px] font-[700] leading-tight">
           Home <br class="max-lg:hidden"/>of Luxury
         </h1>
-        <p class="tracking-wide text-white font-[600] max-sm:text-[10px] md:w-80 sm:w-1/2 w-3/4 italic">"Luxury is when it seems flawless, when you reach the right balance between all elements. Understated theatricality – that is what my luxury is all about."<br/> Jean-Louis Deniot</p>          
+        <p class="tracking-wide text-white font-[600] max-sm:text-[10px] sm:w-1/2 w-1/3 italic">
+          "Luxury is when it seems flawless, when you reach the right balance between all elements. Understated theatricality – that is what my luxury is all about."<br/> Jean-Louis Deniot</p>          
       </div>
       <RouterLink
         id="go-to-portfolio-page"
@@ -18,9 +19,9 @@ import Socials from '../components/Socials.vue'
         to="/portfolio"
         :activeClass="'bg-olive text-white'"
         :exactActiveClass="'bg-olive text-white'"
-        class="w-fit cursor-pointer px-4 py-3 mb-2 font-[400] text-center rounded-[2px] bg-white hover:bg-olive hover:text-white uppercase"
+        class="w-fit cursor-pointer px-4 py-3 mb-2 font-[400] max-sm:p-1 max-sm:text-[10px] text-center rounded-[2px] bg-white hover:bg-olive hover:text-white uppercase"
         >View Projects</RouterLink>
-    <Socials class="mt-auto mb-10"/>
+    <Socials class="mt-auto mb-10 max-sm:hidden"/>
     </div>
   <svg width="5" height="116" viewBox="0 0 5 116" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute mx-auto transform translate-x-1/2 top-[93%] left-1/2 z-[2]">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M1.66675 0.85498H2.77786V114.86H1.66675V0.85498Z" fill="#CCBB99" stroke="#CCBB99"/>
--- src/components/InnerHero.vue
+++ src/components/InnerHero.vue
@@ -6,13 +6,22 @@ defineProps({
 })
 </script>
 <template>
-  <section class="relative flex flex-col items-center justify-center mt-[86px] lg:min-h-[70vh] sm:min-h-[40vh] min-h-full sm:mb-0 mb-28">
-    <img :src="pagebanner" :alt="'Bissar Concepts - ' + pagetitle" width="1440" height="639" loading="eager" class="aspect-2.25/1 absolute inset-0 z-[-1] object-center object-cover 2xl:w-full 2xl:h-full"/>
-    <div class="absolute w-11/12 mx-auto transform sm:space-y-3 2xl:w-8/12 lg:w-10/12 md:-translate-y-1/2 md:top-1/2 top-2/3 -translate-y-2/3">
-      <h1 class="text-white uppercase lg:text-[48px] md:text-[32px] text-[14px] text-center font-[700] leading-tight">
+  <section class="relative flex items-center justify-center mt-[86px] min-h-[60vh] lg:min-h-[60vh] 2xl:min-h-[70vh]">
+    <img 
+      :src="pagebanner" 
+      :alt="'Bissar Concepts - ' + pagetitle" 
+      width="1440" 
+      height="639" 
+      loading="eager" 
+      class="absolute inset-0 object-cover object-center w-full h-full z-[-1] aspect-2.25/1"
+    />
+    <div class="relative top-0 flex flex-col items-center justify-center w-11/12 mx-auto space-y-3 text-center transform sm:space-y-3 2xl:w-8/12 lg:w-10/12">
+      <h1 class="text-white uppercase lg:text-[48px] md:text-[32px] text-[14px] font-[700] leading-tight">
         {{ pagetitle }}
       </h1>
-      <p class="tracking-wide text-white font-[600] max-sm:text-[10px] text-center 2xl:w-1/2 w-3/4 mx-auto">{{ pagedescription }}</p>          
+      <p class="tracking-wide text-white font-[600] max-sm:text-[10px] w-3/4 mx-auto 2xl:w-1/2">
+        {{ pagedescription }}
+      </p>
     </div>
  </section>
 </template>
