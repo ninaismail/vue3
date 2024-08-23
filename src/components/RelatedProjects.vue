c3ae8490c6f6b0ec4a51db59fcda6da203eb2796
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-08-23 21:21:39 +0200

    added the pp title without hold on tablet and mobile

--- src/components/RelatedProjects.vue
+++ src/components/RelatedProjects.vue
@@ -38,14 +38,14 @@ const filteredData = computed(() => {
               class="aspect-0.98/1"
               loading="lazy"
             />
-
-          <div v-if="isHover === key && cat === 'portfolio'" class="flex flex-col justify-center mx-auto gap-y-4 min-h-[20vh] absolute inset-0 w-full h-full bg-[#53554A] bg-opacity-77 px-6">
-            <h1 class="relative text-white lg:text-[24px] font-[700] pb-2 before:absolute before:bottom-0 before:-left-6 before:w-full before:h-[4px] before:bg-gold">
-              {{ item.title }}
-            </h1>
-            <h2 class="tracking-wide text-white font-[600]">{{ item.location }}&nbsp;{{ item.year }}</h2>
-          </div>
-
+            <div v-if="cat === 'portfolio'" 
+            class="flex flex-col justify-center mx-auto gap-y-4 transition-all duration-500 absolute left-0 bottom-0 w-full z-1 bg-[#53554A] bg-opacity-77 px-6"
+            :class="isHover === key ? 'h-full' : 'h-[35%]'">
+                <h1 class="relative text-white lg:text-[24px] font-[700] pb-2 before:absolute before:bottom-0 before:-left-6 before:w-full before:h-[4px] before:bg-gold">
+                    {{ item.title }}
+                </h1>
+                <h2 class="tracking-wide text-white font-[600]">{{ item.location }}&nbsp;{{ item.year }}</h2>   
+            </div>
           <div v-else-if="cat === 'collabs'" class="absolute flex flex-col justify-center w-3/4 text-center transform -translate-x-1/2 bg-white border -bottom-6 h-fit border-gold left-1/2">
             <h1 class="lg:text-[20px] p-2 font-[700] border-b border-gold">
               {{ item.title }}
