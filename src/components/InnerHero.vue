b7b7569acbc05a6bd94bc2ee8889e23587774487
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-08-30 19:25:33 +0300

    all projects

--- src/components/InnerHero.vue
+++ src/components/InnerHero.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-<section class="relative w-full h-full flex items-center justify-center lg:mt-[86px] mt-[74px] aspect-2.25/1" width="1440" height="639" loading="eager" decoding="async"
+<section class="relative w-full h-full flex items-center justify-center lg:mt-[86px] mt-[74px] aspect-2.25/1" loading="eager" decoding="async"
   :style="{ backgroundImage: 'url(' + pagebanner + ')', backgroundSize:'cover', backgroundPosition: collabs ?  'bottom' : 'center', backgroundSize: 'cover', imageRendering: 'crisp-edges', imageResolution: '300dpi' }">
    <div class="w-full h-full absolute inset-0 bg-[#000000]/50 z-1"/>
   <div class="relative flex flex-col items-center justify-center w-11/12 h-full mx-auto space-y-3 text-center sm:space-y-3 2xl:w-8/12 lg:w-10/12">
